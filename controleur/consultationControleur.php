27761588c01a1693906ec560badb18292fa8f2e2
Florian <florian.villevieille@cpe.fre>
2021-09-27 15:19:44 +0200

    le controler on avance mamene

--- controleur/consultationControleur.php
+++ controleur/consultationControleur.php
@@ -1,5 +1,5 @@
 <?php
- 
+    
     $formConsultation = $_POST['formConsultation'] ?? array();
 
     $consultationDonnees = Patho::SearchPatho();
@@ -7,6 +7,29 @@
    if($formConsultation != '')
 
 
+    function consultation(){
+        $smarty=new Smarty;
+        $smarty-display('../vue/consultation/consultation.php');
+    }
 
+    function login(){
+        $smarty=new Smarty;
+        $smarty-display('../vue/index.php');
+    }
+
+
+    function inscript(){
+        $smarty=new Smarty;
+        $smarty-display('../vue/inscription/inscription.php');
+    }
+
+    function recheh(){
+        $smarty=new Smarty;
+        $smarty-display('../vue/recherche/recherche.php');
+    }
+
+
+
+    
 
 ?>
