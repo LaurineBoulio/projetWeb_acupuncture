b2e5be7def876e8bc6fc8a52d20f4ed9c1a1c67e
Clotilde-Long <longclotilde@gmail.com>
2021-11-08 16:33:58 +0100

    test

--- templates_c/66907492237d7f4bd63fae4a57e8e6e5dac76f8d_0.file.index.html.php
+++ templates_c/66907492237d7f4bd63fae4a57e8e6e5dac76f8d_0.file.index.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-11-08 14:57:35
+/* Smarty version 3.1.39, created on 2021-11-08 15:33:07
   from 'D:\wamp64\www\TIDAL\projetWeb_acupuncture\vue\index.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61893adf846012_51750481',
+  'unifunc' => 'content_61894333e959e0_90762111',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '66907492237d7f4bd63fae4a57e8e6e5dac76f8d' => 
     array (
       0 => 'D:\\wamp64\\www\\TIDAL\\projetWeb_acupuncture\\vue\\index.html',
-      1 => 1636381118,
+      1 => 1636385548,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../sharedfile/template.html' => 1,
   ),
 ),false)) {
-function content_61893adf846012_51750481 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61894333e959e0_90762111 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
 <?php $_smarty_tpl->_subTemplateRender("file:../sharedfile/template.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
