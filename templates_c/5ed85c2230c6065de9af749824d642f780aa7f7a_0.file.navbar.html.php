f656d56851857c963b4f661270876abddcc433b3
Clotilde-Long <longclotilde@gmail.com>
2021-11-08 16:45:07 +0100

    modif navbar

--- templates_c/5ed85c2230c6065de9af749824d642f780aa7f7a_0.file.navbar.html.php
+++ templates_c/5ed85c2230c6065de9af749824d642f780aa7f7a_0.file.navbar.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-11-08 14:57:35
+/* Smarty version 3.1.39, created on 2021-11-08 15:44:38
   from 'D:\wamp64\www\TIDAL\projetWeb_acupuncture\sharedfile\navbar.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61893adf8e2a92_38250715',
+  'unifunc' => 'content_618945e69e7715_67815447',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5ed85c2230c6065de9af749824d642f780aa7f7a' => 
     array (
       0 => 'D:\\wamp64\\www\\TIDAL\\projetWeb_acupuncture\\sharedfile\\navbar.html',
-      1 => 1636381118,
+      1 => 1636386268,
       2 => 'file',
     ),
   ),
@@ -20,24 +20,28 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61893adf8e2a92_38250715 (Smarty_Internal_Template $_smarty_tpl) {
-?>
-<link rel="stylesheet" href="/vue/sharedfile/navbar.css">
-<div class="topnav">
-    <a href="#">
-        <img class="logo" src="/src/TravelLogo.png" alt="Logo Travel, inc." />
-    </a>
-    <a class="liens" id="principal" href="#">
-        <img class="picto" src="/src/home-solid.svg" alt="Pictogramme home" />
-        Menu
-    </a>
-    <a class="liens" href="#">
-        <img class="picto" src="/src/concierge-bell-solid.svg" alt="Pictogramme reservation" />
-        consultation
-    </a>
-    <a class="liens" href="#">
-        <img class="picto" src="/src/address-book-regular.svg" alt="Pictogramme contact" />
-        Inscription
-    </a>
-</div><?php }
+function content_618945e69e7715_67815447 (Smarty_Internal_Template $_smarty_tpl) {
+?><link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet"  href="./css/bootstrap.css">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand top" href="#">
+				<img src="./img/logo.png" alt="">
+			</a>
+		</div>
+
+		<div class="collapse navbar-collapse" id="main-menu">
+		   <ul class="nav navbar-nav navbar-right">
+			<li><a href="./index.php?action=getAcc">Accueil</a></li>
+			<li><a href="./index.php?action=getCons">Consulter les pathologies</a></li>
+			<li><a href="./index.php?action=getRecheh">Recherche par mots-clés</a></li>
+			<li><a href="./index.php?action=getIns">S'inscrire</a></li>
+			<li><a href="./index.php?action=getLog">Se connecter</a></li>
+			<li><a href="">Se déconnecter</a></li>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav><?php }
 }
